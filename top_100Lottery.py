8d152f57edb83e0ee0a53b8d1e6834af9408e50e
mrgianhacker <mrgianhacker@gmail.com>
2023-07-25 14:55:07 +0530

    addition of top_100Lottery.py file
    new logic for the lottery results problem is added

---
+++ top_100Lottery.py
@@ -0,0 +1,10 @@
+from random import randint
+
+n = int(input("Enter tha maximum range of winners from the pool: "))
+result_set = set()
+while n > 0:
+    result_set.add(randint(1, 10000000))
+    n = n - 1
+
+print(result_set)
+
